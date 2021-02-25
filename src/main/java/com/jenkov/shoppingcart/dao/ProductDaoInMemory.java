fc22a793fe78a27e50451de81ce258ef8c679e47
Jakob Jenkov <jakob@jenkov.com>
2021-02-25 14:10:56 +0100

    URL corrected in README.md

--- src/main/java/com/jenkov/shoppingcart/dao/ProductDaoInMemory.java
+++ src/main/java/com/jenkov/shoppingcart/dao/ProductDaoInMemory.java
@@ -7,6 +7,12 @@ public class ProductDaoInMemory implements ProductDao {
 
     private Map<String, String> productNames = new HashMap<>();
 
+    public ProductDaoInMemory (){
+        this.productNames.put("prodId", "Default Product");
+        this.productNames.put("prodId1", "Product 1");
+        this.productNames.put("prodId2", "Product 2");
+        this.productNames.put("prodId3", "Product 3");
+    }
 
     public void putProductName(String productId, String productName) {
         this.productNames.put(productId, productId);
